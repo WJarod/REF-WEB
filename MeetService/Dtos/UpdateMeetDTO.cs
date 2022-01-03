953cb43905b9f797ca91beb75fafbda912448309
Jarod Wuillaume <wuillaume.ja@gmail.com>
2022-01-03 16:34:39 +0100

    MeetService (interface , repo , context , dtos , Profile , Addscoped , controllers)

--- MeetService/Dtos/UpdateMeetDTO.cs
+++ MeetService/Dtos/UpdateMeetDTO.cs
@@ -1,19 +1,14 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace MeetService.Models
+namespace MeetService.Dtos
 {
-    public class Meet
-    {
-        [Key]
+    public class UpdateMeetDTO
+    {   
         [Required]
         public int Id { get; set; }
 
         [Required]
         public DateTime Date { get; set; }
-
-        // public User
-
-        // public Client
     }
 }
--- MeetService/Startup.cs
+++ MeetService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeetService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,9 @@ namespace MeetService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // Ajout du context pour acceder à la base de donnée en local 
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("Meet"));
+            services.AddScoped<IMeetRepo, MeetRepo>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddControllers();
             services.AddSwaggerGen(c =>
