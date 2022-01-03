7c5144f801b3177b958f8434a4371ef723a4d2c7
Jarod Wuillaume <wuillaume.ja@gmail.com>
2022-01-03 11:43:36 +0100

    AppDbContext
    Création de notre context

--- ClientService/Startup.cs
+++ ClientService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClientService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +31,9 @@ namespace ClientService
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddControllers();
+            
+            // Ajout du context pour acceder à la base de donnée en local 
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("Client"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClientService", Version = "v1" });
